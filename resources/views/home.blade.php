664b3ca8a9606fa42f984c692915c41f62df9230
Bảo Hân <90457766+baohan93@users.noreply.github.com>
2022-05-11 15:06:09 +0700

    enter

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,7 +57,7 @@
                         <a class="nav-link" href="#contact">Contact</a>
                     </li>
                     <li class="nav-item">
-                         <a class="nav-link" href="{{ route('login') }}">Login  /</a>
+                         <a class="nav-link" href="{{ route('login') }}">Login  |</a>
                     </li>
                     <li class="nav-item">
                          <a class="nav-link" href="{{ route('register') }}">Register</a>
